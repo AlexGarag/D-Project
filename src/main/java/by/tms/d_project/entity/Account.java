56a7dd59d47587a9bf551851620e4f1295a4d780
alex garag <alex.garag@bk.ru>
2025-01-06 17:18:36 +0300

    Создал аккаунт

---
+++ src/main/java/by/tms/d_project/entity/Account.java
@@ -0,0 +1,24 @@
+package by.tms.d_project.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+//import org.hibernate.annotations.Fetch;
+//import org.hibernate.annotations.FetchMode;
+
+//import java.util.HashSet;
+//import java.util.Set;
+
+@Entity
+@Data
+@ToString
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/by/tms/d_project/service/AccountService.java
@@ -0,0 +1,53 @@
+package by.tms.d_project.service;
+
+import by.tms.d_project.entity.Account;
+import by.tms.d_project.repository.AccountRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account create(Account account) {
+        account.setPassword(new BCryptPasswordEncoder(11).encode(account.getPassword()));
+        accountRepository.save(account);
+        return account;
+    }
+
+    public Account checkAccount(String username) {
+        Optional<Account> account = accountRepository.findByUsername(username);
+        if (account.isPresent()) {
+            return account.get();
+        } else {
+            throw new EntityNotFoundException("Account not found");
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Account> account = accountRepository.findByUsername(username);
+        if (account.isPresent()) {
+            var acc = account.get();
+            return User
+                    .withUsername(acc.getUsername())
+                    .password(acc.getPassword())
+                    .roles("USER")
+                    .build();
+        }
+        throw new UsernameNotFoundException(username);
+    }
+}
