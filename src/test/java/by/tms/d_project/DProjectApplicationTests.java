8c4d13271da8a0a48d77b4a8463c08d4bf268f11
alex garag <alex.garag@bk.ru>
2025-01-06 16:11:39 +0300

    initialization

---
+++ src/test/java/by/tms/d_project/DProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.d_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
